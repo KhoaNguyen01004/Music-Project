0673db49a26e430770585fed8a603346535018c8
Kevin Nguyen <khoa01004@gmail.com>
2022-11-08 19:31:02 -0500

    init

--- src/Main.py
+++ src/Main.py
@@ -1,17 +1,16 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-client_id = "e92db17472c34f5294229fdb3cf08ccb"
-client_secret = "88e6b43105cb448290290f0e837d0f90"
+client_id = "5d2671fe13644512aeb70d497c165c82"
+client_secret = "4ae2f73b3aab4106903bc34919f04043"
 
 client_credentials_manager = SpotifyClientCredentials(
     client_id=client_id, client_secret=client_secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 play_list = "https://open.spotify.com/playlist/37i9dQZF1DZ06evO1rVRqp?si=e20f8b3bd52c4427"
-items = sp.playlist_items(playlist_id=play_list)["items"]
+items = sp.playlist_tracks(playlist_id=play_list)["items"]
 
 for i in items:
-    print(i)
-    break
-
+    artist_info = i["track"]
+    print(f"{i['track']['name']}, {artist_info['artists'][0]['name']}")
