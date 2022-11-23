3464d8fce512262f50ed49f168ec570f202e78c2
Kevin Nguyen <khoa01004@gmail.com>
2022-11-22 19:57:35 -0500

    add docstring for add_stop_word method

--- src/WordCloudHandler.py
+++ src/WordCloudHandler.py
@@ -12,4 +12,9 @@ class WordCloudHandler:
         self.wc.to_file(filename="wordcloud.png")
 
     def add_stop_word(self, word: str):
+        """
+        Description:
+        ------------
+        This will add a stop word to a list if it not yet in stop word list
+        """
         wordcloud.STOPWORDS.add(word)
