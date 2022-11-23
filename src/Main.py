df103c69e6a2f3d71c1ff97284a0a45e0ab4cf7e
Kevin Nguyen <khoa01004@gmail.com>
2022-11-22 19:27:42 -0500

    use spotify and wordcloud to generate wordcloud

--- src/Main.py
+++ src/Main.py
@@ -1,8 +1,13 @@
-from SpotifyHandler import SpotifyHandler as sp
+import SpotifyHandler
+import WordCloudHandler
+
 
 def main():
-    sp.init_play_list(url_link="https://open.spotify.com/playlist/37i9dQZEVXbMDoHDwVN2tF?si=30a40fcfc3a74d33")
-    print()
+    sp = SpotifyHandler.SpotifyHandler()
+    sp.init_play_list(
+        url_link="https://open.spotify.com/playlist/37i9dQZEVXbMDoHDwVN2tF?si=30a40fcfc3a74d33")
+    wc = WordCloudHandler.WordCloudHandler(text=sp.to_string())
+    wc.wordcloud_to_img()
 
 
 if __name__ == "__main__":
